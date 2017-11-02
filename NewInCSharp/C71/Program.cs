adab5da847cd534ae9790f0304f1b1e5af80b1a0
Nazar <dutka.nazar@gmail.com>
2017-11-02 23:23:11 +0200

    new in c# 7.1

---
+++ NewInCSharp/C71/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace C71
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var results = Enumerable.Range(1, 40)
+                .Select(input => (input,task: FibonacciAsync(input)))
+                .ToArray();
+
+            foreach (var item in results)
+            {
+                WriteLine($"Fib {item.input} = {await item.task}");
+            }
+            ReadLine();
+        }
+
+        private static Task<int> FibonacciAsync (int n, CancellationToken token = default)
+        {
+            return Task.Run(() => Fib(n).curr, token);
+
+            (int curr,int prev) Fib(int i)
+            {
+                if (i is 0) return (1, 0);
+                var (c, p) = Fib(i - 1);
+                return (c + p, c);
+            }
+        }
+    }
+}
