a7616ba11ad5646325a6ad77c9afb73d16c629a5
Nazar <dutka.nazar@gmail.com>
2017-11-26 21:12:19 +0200

    private protected

--- NewInCSharp/C72/Program.cs
+++ NewInCSharp/C72/Program.cs
@@ -77,4 +77,25 @@ namespace C72
         public Double Y { get;  }
         public Double Z { get;  }
     }
+
+    // 7.2 private protected 
+    public class BaseClass
+    {
+        private protected int myValue = 0;
+    }
+
+    public class DerivedClass1 : BaseClass
+    {
+        void Access()
+        {
+            BaseClass baseObject = new BaseClass();
+
+            // Error CS1540, because myValue can only be accessed by
+            // classes derived from BaseClass.
+            // baseObject.myValue = 5;  
+
+            // OK, accessed through the current derived class instance
+            myValue = 5;
+        }
+    }
 }
--- NewInCSharp/C80Net/Person.cs
+++ NewInCSharp/C80Net/Person.cs
@@ -30,4 +30,15 @@ namespace C80Net
             //return name?.Length ?? 0;
         }
     }
+    // Assembly2.cs  
+    // Compile with: /reference:Assembly1.dll  
+    class DerivedClass2 : BaseClass
+    {
+        void Access()
+        {
+            // Error CS0122, because myValue can only be
+            // accessed by types in Assembly1
+            // myValue = 10;
+        }
+    }
 }
